e9d27ea0b362d78b1b95ad84c1acc4a4c51706b2
LeoUraltsev <leouraltsev@gmail.com>
2025-03-10 19:44:35 +0200

    Добавлены обработчики для роутов

---
+++ internal/domain/errors.go
@@ -0,0 +1,7 @@
+package domain
+
+import "errors"
+
+var (
+	ErrScheduleNotFound = errors.New("schedule not found")
+)
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,40 +3,25 @@ package handlers
 import (
 	"github.com/LeoUraltsev/medication-reminder/internal/config"
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/render"
-	"net/http"
+	"log/slog"
 )
 
 type Handler struct {
+	s      Services
 	router chi.Router
 	cfg    *config.App
+	log    *slog.Logger
 }
 
-func New(r chi.Router, cfg *config.App) *Handler {
-	return &Handler{router: r, cfg: cfg}
+func New(s Services, r chi.Router, cfg *config.App, log *slog.Logger) *Handler {
+	return &Handler{s: s, router: r, cfg: cfg, log: log}
 }
 
 func (h Handler) InitHandlers() {
 	h.router.Route("/api/v1", func(r chi.Router) {
 		r.Post("/schedule", h.CreateSchedule)
 		r.Get("/schedules", h.SchedulesIds)
-		r.Get("/schedule", h.ScheduleReception)
+		r.Get("/schedule", h.Schedule)
 		r.Get("/next_takings", h.NextTakings)
 	})
-}
-
-func (h Handler) CreateSchedule(w http.ResponseWriter, r *http.Request) {
-	render.JSON(w, r, "test create")
-}
-
-func (h Handler) SchedulesIds(w http.ResponseWriter, r *http.Request) {
-	render.JSON(w, r, "test get ids schedules")
-}
-
-func (h Handler) ScheduleReception(w http.ResponseWriter, r *http.Request) {
-	render.JSON(w, r, "test get schedule")
-}
-
-func (h Handler) NextTakings(w http.ResponseWriter, r *http.Request) {
-	render.JSON(w, r, "test get next takings")
 }
